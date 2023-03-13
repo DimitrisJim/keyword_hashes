f2decd9b5773c379da6d6752793474a404dba28f
DimitrisJim <d.f.hilliard@gmail.com>
2023-03-14 00:32:30 +0200

    Add stdlib hashmap option.

--- benches/compare_hashes.rs
+++ benches/compare_hashes.rs
@@ -1,22 +1,43 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use kwhash::phf_is_keyword;
+use kwhash::{phf, stdlib_hashmap};
+use unic_ucd_ident::is_xid_start;
 
 // Tokenize the pydump.txt file containing the full stdlib and the test lib.
-// TODO: Maybe filter by idents because that's what we basically do.
-fn tokenize_py() -> impl Iterator<Item = &'static str> {
+// We only do a rudimentary tokenization by splitting on whitespace and filtering
+// out non-identifiers. The effect of this is basically as if we had more non-keywords
+// than what we usually would.
+fn rudimentary_tokens() -> Vec<&'static str> {
     let pydump = include_str!("pydump.txt");
-    pydump.split_whitespace()
+    pydump
+        .split_whitespace()
+        .filter(|s| {
+            let c = s.chars().next().unwrap();
+            match c {
+                'a'..='z' | 'A'..='Z' | '_' => true,
+                _ => is_xid_start(c),
+            }
+        })
+        .collect()
 }
 
 // Bench alternatives:
 fn bench_alts(c: &mut Criterion) {
-    // phf
-    let tokens: Vec<_> = tokenize_py().collect();
-    c.bench_function("phf", |b| {
+    let tokens: Vec<_> = rudimentary_tokens();
+
+    c.bench_function(&format!("phf: ({0} keywords)", tokens.len()), |b| {
         let tokens = tokens.clone();
         b.iter(|| {
             for t in &tokens {
-                phf_is_keyword(t);
+                phf::is_keyword(t);
+            }
+        })
+    });
+
+    c.bench_function(&format!("stdlib hashmap: ({0} keywords)", tokens.len()), |b| {
+        let tokens = tokens.clone();
+        b.iter(|| {
+            for t in &tokens {
+                stdlib_hashmap::is_keyword(t);
             }
         })
     });
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,71 @@
 //! Export functions for benching.
 mod token;
-use token::Tok;
 
-// generated in build.rs, in gen_phf()
-/// A map of keywords to their tokens.
-pub static KEYWORDS: phf::Map<&'static str, Tok> =
-    include!(concat!(env!("OUT_DIR"), "/keywords.rs"));
+// Rust phf option. Generated in build.rs, in gen_phf()
+pub mod phf {
+    use crate::token::*;
 
-#[inline(always)]
-pub fn phf_is_keyword(s: &str) -> bool {
-    KEYWORDS.contains_key(s)
+    pub static KEYWORDS: phf::Map<&'static str, Tok> =
+        include!(concat!(env!("OUT_DIR"), "/keywords.rs"));
+
+    #[inline(always)]
+    pub fn is_keyword(s: &str) -> bool {
+        KEYWORDS.contains_key(s)
+    }
+}
+
+// Normal hashmap.
+pub mod stdlib_hashmap {
+    use crate::token::*;
+
+    use once_cell::sync::Lazy;
+    use std::collections::HashMap;
+
+    static KEYWORDS: Lazy<HashMap<&'static str, Tok>> = Lazy::new(|| {
+        let mut m = HashMap::new();
+        m.insert("...", Tok::Ellipsis);
+        m.insert("False", Tok::False);
+        m.insert("None", Tok::None);
+        m.insert("True", Tok::True);
+        m.insert("and", Tok::And);
+        m.insert("as", Tok::As);
+        m.insert("assert", Tok::Assert);
+        m.insert("async", Tok::Async);
+        m.insert("await", Tok::Await);
+        m.insert("break", Tok::Break);
+        m.insert("case", Tok::Case);
+        m.insert("class", Tok::Class);
+        m.insert("continue", Tok::Continue);
+        m.insert("def", Tok::Def);
+        m.insert("del", Tok::Del);
+        m.insert("elif", Tok::Elif);
+        m.insert("else", Tok::Else);
+        m.insert("except", Tok::Except);
+        m.insert("finally", Tok::Finally);
+        m.insert("for", Tok::For);
+        m.insert("from", Tok::From);
+        m.insert("global", Tok::Global);
+        m.insert("if", Tok::If);
+        m.insert("import", Tok::Import);
+        m.insert("in", Tok::In);
+        m.insert("is", Tok::Is);
+        m.insert("lambda", Tok::Lambda);
+        m.insert("match", Tok::Match);
+        m.insert("nonlocal", Tok::Nonlocal);
+        m.insert("not", Tok::Not);
+        m.insert("or", Tok::Or);
+        m.insert("pass", Tok::Pass);
+        m.insert("raise", Tok::Raise);
+        m.insert("return", Tok::Return);
+        m.insert("try", Tok::Try);
+        m.insert("while", Tok::While);
+        m.insert("with", Tok::With);
+        m.insert("yield", Tok::Yield);
+        m
+    });
+
+    #[inline(always)]
+    pub fn is_keyword(s: &str) -> bool {
+        KEYWORDS.contains_key(s)
+    }
 }
